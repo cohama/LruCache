8d2ed5c53ce457d0b05c6823188b5480559885fe
cohama <hirotaka.hamada.1024@gmail.com>
2011-03-06 09:36:39 +0900

    最初のコミット。

---
+++ LruCache.Test/LruCacheTest.fs
@@ -0,0 +1,45 @@
+﻿// F# の詳細 (http://fsharp.net)
+
+module LruCacheTest
+
+open FsUnit
+open NUnit.Framework
+open LruCache
+
+[<TestFixture>]
+type キャッシュテスト() =
+    
+    let empty_cache = LruCache<_, _>()
+    
+    [<Test>]
+    member this.必ず成功するスタブ() = true |> should be True
+
+    [<Test>]
+    member this.空のデータが登録できる() = 
+        empty_cache.List |> should equal []
+
+    member private this.IsSameElementList (xs: 'a list) (ys: 'a list) = 
+        Set xs |> should equal (Set ys)
+    
+    // listの内容を持つLRUキャッシュをputの連続で作成する
+    member private this.DataSet xs = 
+        List.fold (fun cache (k, v) -> cache |> put k v) empty_cache xs
+
+    [<Test>]
+    member this.複数のデータが登録できる() = 
+        let ok xs ys =
+            this.IsSameElementList (xs |> this.DataSet |> to_list) ys
+        ok [("key", "value")] [("key", "value")] 
+//        this.IsSameElementList ((this.DataSet [("a","b"); ("c","d")]).List) [("a", "b"); ("c", "d")]
+//        ((this.DataSet ["a","b"; "c","d"]).List, [("a", "b"); ("c", "d")])
+//        ||> this.IsSameElementList
+
+    [<Test>]
+    member this.登録されているデータが取り出せる() =
+        this.DataSet [] |> get "key" |> should equal None
+        this.DataSet [("key", "value")] |> get "key" |> should equal (Some "value")
+
+    [<Test>]
+    member this.Size以上の要素を追加した場合に最初の要素が切り捨てられる() = 
+        [("b", "B"); ("c", "C"); ("d", "D")] 
+        |> this.IsSameElementList ((this.DataSet [("a", "A"); ("b", "B"); ("c", "C"); ("d", "D")]).List)
---
+++ LruCache/Program.fs
@@ -0,0 +1,17 @@
+﻿module LruCache
+
+type LruCache<'a, 'b>( lst: ('a*'b) list, size: int) =
+    member this.Size = size
+    member this.List : ('a*'b) list = lst
+    new () = new LruCache<'a, 'b>( [], 3 )
+    
+let put k v (lst:LruCache<_, _>) =
+    
+    new LruCache<_, _>( (k, v)::lst.List, lst.Size)
+
+let get k (lst: LruCache<_, _>) = 
+    match List.tryFind (fun (key,_) -> k = key) lst.List with
+     | Some(_,v) -> Some(v)
+     | _ -> None 
+
+let to_list (x : LruCache<_, _>) = x.List
